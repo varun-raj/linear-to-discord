16a82dce1d7a3de435eb9840b9afcf95bb6ad6cf
Varun Raj <varun@skcript.com>
2023-07-02 22:34:27 +0530

    fix: cypress fix
    Signed-off-by: Varun Raj <varun@skcript.com>

--- src/lib/linear.ts
+++ src/lib/linear.ts
@@ -1,23 +1,23 @@
-import { ILinearData } from "@/@types";
-import { ERRORS } from "@/config/appConstants";
-import { createCommentParser } from "./linearActions/commentChanges";
-import { createIssueParser, updateIssueParser } from "./linearActions/issueChanges";
+import type { ILinearData } from '@/@types';
+import { ERRORS } from '@/config/appConstants';
 
+import { createCommentParser } from './linearActions/commentChanges';
+import {
+  createIssueParser,
+  updateIssueParser,
+} from './linearActions/issueChanges';
 
 export const parseData = (data: ILinearData<any>): string => {
   const { action, type } = data;
   switch (action) {
-    case "create":
-      if (type === "Issue") 
-      return createIssueParser(data);
-      else if (type === "Comment")
-      return createCommentParser(data);
-      else return ERRORS.UNKNOWN_ACTION;
-    case "update":
-      if (type === "Issue")
-        return updateIssueParser(data);
+    case 'create':
+      if (type === 'Issue') return createIssueParser(data);
+      if (type === 'Comment') return createCommentParser(data);
+      return ERRORS.UNKNOWN_ACTION;
+    case 'update':
+      if (type === 'Issue') return updateIssueParser(data);
       return ERRORS.UNKNOWN_ACTION;
     default:
       return ERRORS.UNKNOWN_ACTION;
   }
-}
+};
--- src/pages/api/whisper.ts
+++ src/pages/api/whisper.ts
@@ -1,15 +1,19 @@
-import { ERRORS } from "@/config/appConstants";
-import { sendMessage } from "@/lib/discord";
-import { parseData } from "@/lib/linear";
-import { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+import { ERRORS } from '@/config/appConstants';
+import { sendMessage } from '@/lib/discord';
+import { parseData } from '@/lib/linear';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const data = req.body;
-  const message = parseData(data)
+  const message = parseData(data);
   if (message === ERRORS.UNKNOWN_ACTION) {
     return res.json({ success: false, message: ERRORS.UNKNOWN_ACTION });
   }
   return sendMessage(message)
-  .then((data) => res.json({ success: true }))
-  .catch((err) => res.json({ success: false, message: err.message,}));
+    .then(() => res.json({ success: true }))
+    .catch((err) => res.json({ success: false, message: err.message }));
 }
